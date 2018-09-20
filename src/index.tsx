ddf1e797f1d8fefa0c235e87cdaf265a933a624d
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-09-20 15:21:45 +0300

    Added the actual code

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,96 @@
 
 import * as React from "react";
 
-import styles from "./styles.css";
+import {
+  Wrapper,
+  ResultCounter,
+  Selected,
+  SelectedRemove,
+  Input,
+  Result,
+  ResultItem,
+  ResultOverRender
+} from "./helpers";
 
-export type Props = { text: string };
+export interface DataItem {
+  value: any;
+  label: string;
+  searchable: string;
+}
+
+export type Props = {
+  data: Array<DataItem>;
+  onChange: Function;
+  placeholder: string | undefined;
+  wrapperClass?: any;
+  className?: any;
+};
 
 export default class ReactTachiSelect extends React.Component<Props> {
-  render() {
-    const { text } = this.props;
+  public state = {
+    label: null,
+    search: ""
+  };
 
-    return <div className={styles.test}>Example Component: {text}</div>;
+  onChange = (item: DataItem | null) => {
+    this.props.onChange(item ? item.value : null);
+    this.setState({ label: item ? item.label : null, search: "" });
+  };
+
+  onType = (e: any) => {
+    this.setState({ search: e.target.value });
+  };
+
+  render() {
+    const { wrapperClass, className, placeholder = "Search" } = this.props;
+
+    let { data } = this.props;
+    const { search, label } = this.state;
+
+    if (search.length > 0) {
+      data = data.filter((o) => o.searchable.includes(search.toLowerCase()));
+    }
+
+    const renderLimit = 250;
+    const overRenderLimit = data.length > renderLimit;
+
+    return (
+      <Wrapper className={wrapperClass}>
+        {!label ? <ResultCounter>({data.length})</ResultCounter> : null}
+        {label ? (
+          <Selected className={className}>
+            {label}
+            <SelectedRemove
+              href="javascript:;"
+              onClick={() => this.onChange(null)}
+            >
+              &times;
+            </SelectedRemove>
+          </Selected>
+        ) : (
+          <Input
+            placeholder={placeholder}
+            type="text"
+            className={className}
+            onChange={this.onType}
+          />
+        )}
+
+        {search.length > 0 ? (
+          <Result>
+            {data.slice(0, renderLimit).map((item: DataItem, idx: number) => (
+              <ResultItem
+                href="javascript:;"
+                key={"react-tachi-select-result-item-" + idx}
+                onClick={() => this.onChange(item)}
+              >
+                {item.label}
+              </ResultItem>
+            ))}
+            {overRenderLimit ? <ResultOverRender>...</ResultOverRender> : null}
+          </Result>
+        ) : null}
+      </Wrapper>
+    );
   }
 }
